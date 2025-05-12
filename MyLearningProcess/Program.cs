9ec1a208b5597908c21186a08316c740e6c128e3
Lucas Melo <lucasmelo.tec.saude@gmail.com>
2025-05-12 19:15:55 -0300

    Learned IComparable

---
+++ MyLearningProcess/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using MyLearningProcess.Entities;
+
+namespace MyLearningProcess {
+    //This project was created to show my process of studying and all that i learned about coding
+    //Learning IComparable
+    class Program {
+        static void Main(string[] args) {
+            string path = @"c:\temp\in.txt"; 
+
+            try {
+                using(StreamReader sr = File.OpenText(path)) {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream) {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list) {
+                        Console.WriteLine(emp);
+                    }
+                } 
+            }
+            catch (IOException ex) {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
